c235630905231b4ca7d6e8b036bfbfcf7cbc1382
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2021-12-18 12:59:36 +0100

    mission page en cours

--- src/app/components/mission/mission.component.ts
+++ src/app/components/mission/mission.component.ts
@@ -1,3 +1,4 @@
+import { isNull } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,6 +11,13 @@ export class MissionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // increment coins
+  reward(coins: number){
+    const currentCoins = localStorage.getItem("coins");
+    const final = Number(currentCoins)!+coins;
+    localStorage.setItem("coins", final.toString());
   }
 
 }

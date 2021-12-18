173a8084371f7ec393bb4f8285ac15c7a327c65b
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2021-12-18 13:24:37 +0100

    Add files via upload

---
+++ src/app/components/mission-two/mission-two.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-two',
+  templateUrl: './mission-two.component.html',
+  styleUrls: ['./mission-two.component.css']
+})
+export class MissionTwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

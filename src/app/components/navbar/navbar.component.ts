ab8d0d2f9e81632fd2852bf498b21a2b4244bed3
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2021-12-21 16:10:24 +0100

    Add files via upload

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,9 +12,30 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
     var energy = Number(localStorage.getItem("energy"));
-    if (energy === null){
-      localStorage.setItem("energy", "5");
+    if (energy == null){
+      localStorage.setItem("energy", "50");
     }
+    if (energy < 30){
+      document.getElementById("elevel")!.style.backgroundColor= "red"
+    }
+    else if (energy < 60){
+      document.getElementById("elevel")!.style.backgroundColor= "orange"
+    }
+    else if (energy == 100){
+      document.getElementById("elevel")!.style.backgroundColor= "lightgreen"
+    }
+    else{
+      document.getElementById("elevel")!.style.backgroundColor= "green"
+    }
+    document.getElementById("elevel")!.style.width = energy.toString()+"px";
+  }
+
+  refresh(){
+    this.ngOnInit();
+  }
+
+  getCoins(): string{
+    return localStorage.getItem("coins")!;
   }
 
 }

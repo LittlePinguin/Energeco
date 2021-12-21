4c2b4c178bda752ed662f70c2637b2b3fdd47522
ClementVallat <37073923+ClementVallat@users.noreply.github.com>
2021-12-21 10:16:39 +0100

    Add files via upload

---
+++ src/app/components/mission-eight/mission-eight.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-eight',
+  templateUrl: './mission-eight.component.html',
+  styleUrls: ['./mission-eight.component.css']
+})
+export class MissionEightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

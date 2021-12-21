d138e526d8a8a7769a41575d47aa5f357f7e87b9
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2021-12-21 22:36:41 +0100

    Add files via upload

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,32 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if (localStorage.getItem("coins")===null) {
+      localStorage.setItem("coins","0");
+    }
+    if (localStorage.getItem("energy")===null) {
+      localStorage.setItem("energy","5");
+    }
+
+    if (Number(localStorage.getItem("witch")) != 1){
+      if (Number(localStorage.getItem("redbot")) != 1){
+        if (Number(localStorage.getItem("ninja")) != 1){
+          if (Number(localStorage.getItem("knight")) != 1){
+            if (Number(localStorage.getItem("hero")) != 1){
+              if (Number(localStorage.getItem("perry")) != 1){
+                if (Number(localStorage.getItem("yebot")) != 1){
+                  if (Number(localStorage.getItem("megalo")) != 1){
+                    if (Number(localStorage.getItem("spiderman")) != 1){
+                      localStorage.setItem("redbot", "1");
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
 }

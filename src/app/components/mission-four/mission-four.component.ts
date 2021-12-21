8a76792af95f9941b60fc76e5aaa33904d675921
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2021-12-21 16:12:23 +0100

    Add files via upload

--- src/app/components/mission-four/mission-four.component.ts
+++ src/app/components/mission-four/mission-four.component.ts
@@ -12,4 +12,22 @@ export class MissionFourComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // increment coins and energy
+  reward(coins: number, energy: number){
+    var currentCoins = localStorage.getItem("coins");
+    var finalCoins = Number(currentCoins)!+coins;
+    if (finalCoins < 0){
+      finalCoins = 0;
+    }
+    localStorage.setItem("coins", finalCoins.toString());
+    
+    var currentEnergy = localStorage.getItem("energy");
+    var finalEnergy = Number(currentEnergy)+energy;
+    if (finalEnergy > 100){
+      finalEnergy = 100;
+      localStorage.setItem("robot", "1");
+    }
+    localStorage.setItem("energy", finalEnergy.toString());
+  }
+
 }
